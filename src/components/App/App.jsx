2f17457aa253fdacb71d0e8bc3085c6547b34067
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-30 14:29:11 -0600

    update: login page working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../HomePage/HomePage'
 import FestivalSchedule from '../FestivalSchedule/FestivalSchedule'
+import LoginPage from '../Login/LoginPage'
 import NavBar from '../NavBar/NavBar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/festivals/:id" element={<FestivalSchedule />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   )
--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -5,17 +5,22 @@ export default function LoginPage() {
   const navigate = useNavigate()
 
   const handleLogin = () => {
-    localStorage.setItem('userId', 1)
-    localStorage.setItem('scheduleId', 1)
+    localStorage.setItem('user', JSON.stringify({
+      id: 1,
+      schedule_id: 1,
+      username: 'johnnyb'
+    }))
     navigate('/')
   }
 
   return (
-    <div className="login-container">
-      <h1>Welcome to Happy Feet 🎶</h1>
-      <button className="login-button" onClick={handleLogin}>
-        Log In as Guest
-      </button>
+    <div className="login-page">
+      <div className="login-container">
+        <h1 className="login-heading">Lets get those Feet Happy & Moving🎶</h1>
+        <button className="login-button" onClick={handleLogin}>
+          Log In as Guest
+        </button>
+      </div>
     </div>
   )
 }
